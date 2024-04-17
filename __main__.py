ed899125c2465d70ac728d406ccf482c883cdff8
Deligny <61823445+LeonDeligny@users.noreply.github.com>
2024-04-17 12:43:18 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,15 +1,10 @@
 import numpy as np, pandas as pd
-import torch, os, logging, optuna
+import torch, os, logging
 
-from sklearn.preprocessing import StandardScaler
-from torch_geometric.data import Data
-from tqdm import tqdm
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 from preprocessing import load_dataset, normalize
-from plot import plot_predictions_vs_test, plot_test
-from PIMNN_Phy_Bc import PIMNN_Phy_Bc
 
 # Set up Python logging
 logging.basicConfig(level=logging.ERROR)
@@ -21,15 +16,15 @@ writer = SummaryWriter(logdir)
 
 pd.set_option('display.precision', 20)
 
-torch.manual_seed(1234)
+# torch.manual_seed(1234)
 torch.autograd.set_detect_anomaly(True)
 
 if __name__ == "__main__":
 
     # Load Data (change path if needed)
-    path = ["airFoil2D_SST_31.68_0.424_0.273_4.301_1.0_11.616"]
-    train_data, bc_data, box_data = load_dataset(path, 
-                                       n_random_sampling = 100
+    path = ["airFoil2D_SST_31.677_-0.742_2.926_5.236_5.651"]
+    train_data, len_list = load_dataset(path, 
+                                       n_random_sampling = 50000
                                        )
 
     Uinf, alpha, gamma_1, gamma_2, gamma_3 = float(path[0].split('_')[2]), float(path[0].split('_')[3])*np.pi/180, float(path[0].split('_')[4]), float(path[0].split('_')[5]), float(path[0].split('_')[6])
@@ -37,46 +32,36 @@ if __name__ == "__main__":
     
     u_inlet, v_inlet = np.cos(alpha)*Uinf, np.sin(alpha)*Uinf
     
-    df_train_input = pd.DataFrame(train_data[0].x_train, columns=["x", "y", "sdf"])
+    df_train_input = pd.DataFrame(train_data[0].x_train, columns=["x", "y", "sdf", "x_n", "y_n"])
     df_train_target = pd.DataFrame(train_data[0].y_train, columns=["u", "v", "p", "nut"])
     df_train = pd.concat([df_train_input, df_train_target], axis=1) 
 
-    df_bc_input = pd.DataFrame(bc_data[0].x_bc, columns=["x", "y", "sdf"])
-    df_bc_target = pd.DataFrame(bc_data[0].y_bc, columns=["u", "v", "p", "nut"])
-    df_bc = pd.concat([df_bc_input, df_bc_target], axis=1) 
+    df_train_n, _ = normalize(df_train)
+    mean_variance_dict = {column: {"mean": 0.0, "var": 1.0} for i, column in enumerate(df_train.columns)}
+    df_aerofoil = df_train.iloc[len_list[0]:len_list[0]+len_list[1],:]
 
-    df_combined = pd.concat([df_train, df_bc], axis=0)
-    df_combined_normalized, mean_variance_dict = normalize(df_combined) # normalize(df_combined)
+    df_freestream_n = df_train_n.iloc[:len_list[0],:]
+    df_aerofoil_n = df_train_n.iloc[len_list[0]:len_list[0]+len_list[1],:]
+    df_box_n = df_train_n.iloc[len_list[0]:len_list[0]+len_list[1]+len_list[2],:]
+        
+    print("Datasets Loaded.")
 
-    l = len(df_train)
+    from FourierNeuralOperatorNN import FourierNeuralOperatorNN
+    from plot import plot_predictions_vs_test, plot_test
 
-    df_train_normalized = df_combined_normalized.iloc[:l, :]
-    df_bc_normalized = df_combined_normalized.iloc[l:, :]
+    # Train the model
+    model = FourierNeuralOperatorNN(df_train, df_aerofoil, mean_variance_dict, len_list, u_inlet, v_inlet, gamma_1, gamma_2, gamma_3)
+    if torch.cuda.device_count() > 1:
+        print(f"Let's use {torch.cuda.device_count()} GPUs!")
+        model = torch.nn.DataParallel(model)
+        
+    # Check for CUDA availability
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    df_box_input = pd.DataFrame(box_data[0].x_box, columns=["x", "y", "sdf"])
-    df_box_target = pd.DataFrame(box_data[0].y_box, columns=["u", "v", "p", "nut"])
-    df_box = pd.concat([df_box_input, df_box_target], axis=1)
+    print(f"Using devide : {device}")
 
-    l_prime = len(df_box)
+    model.to(device)
 
-    df_box_normalized = df_combined_normalized.iloc[:l_prime, :]
-    
-    print("Dataset loaded.")
-    print(mean_variance_dict)
-        # Train the model
-    fourier_scale = 1
-    fourier_mapdim = 64
-
-    model = PIMNN_Phy_Bc(df_train_normalized, df_bc_normalized, mean_variance_dict, u_inlet, v_inlet, gamma_1, gamma_2, gamma_3, fourier_scale, fourier_mapdim)
-    model.pre_train(51)
-    print(f"Finished pre-Training.")
-    model.train(501)
+    print(f"Started Training.")
+    model.train(101)
     print(f"Finished Training.")
-    # Prediction u_pred, v_pred, p_pred, nut_pred
-    u_pred, v_pred, p_pred, nut_pred = model.predict(df_box_normalized, mean_variance_dict, u_inlet, v_inlet, gamma_1, gamma_2, gamma_3)
-
-    # Plotting
-    plot_predictions_vs_test(df_box_normalized['x'].astype(float).values.flatten(), df_box_normalized['y'].astype(float).values.flatten(), u_pred, df_box_normalized['u'], 'u', 'PINN_u_Supervised')
-    plot_predictions_vs_test(df_box_normalized['x'].astype(float).values.flatten(), df_box_normalized['y'].astype(float).values.flatten(), v_pred, df_box_normalized['v'], 'v', 'PINN_v_Supervised')
-    plot_predictions_vs_test(df_box_normalized['x'].astype(float).values.flatten(), df_box_normalized['y'].astype(float).values.flatten(), p_pred, df_box_normalized['p'], 'p', 'PINN_p_Supervised')
-    plot_predictions_vs_test(df_box_normalized['x'].astype(float).values.flatten(), df_box_normalized['y'].astype(float).values.flatten(), nut_pred, df_box_normalized['nut'], 'nut', 'PINN_nut_Supervised')
