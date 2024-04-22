1ba303e25192e5f733b50dda818c19013f86ad2d
Deligny <61823445+LeonDeligny@users.noreply.github.com>
2024-04-22 14:08:11 +0200

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -3,8 +3,10 @@ import torch, os, logging
 
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
+from scipy.interpolate import griddata
+from scipy.spatial import cKDTree
 
-from preprocessing import load_dataset, normalize
+from preprocessing import load_dataset, normalize, compute_minimum_distances
 
 # Set up Python logging
 logging.basicConfig(level=logging.ERROR)
@@ -18,13 +20,14 @@ pd.set_option('display.precision', 20)
 
 # torch.manual_seed(1234)
 torch.autograd.set_detect_anomaly(True)
+#torch.set_printoptions(edgeitems=1000)
 
 if __name__ == "__main__":
 
     # Load Data (change path if needed)
-    path = ["airFoil2D_SST_31.68_0.424_0.273_4.301_1.0_11.616"]
+    path = ["airFoil2D_SST_31.677_-0.742_2.926_5.236_5.651"]
     train_data, len_list = load_dataset(path, 
-                                       n_random_sampling = 50000
+                                       n_random_sampling = 0
                                        )
 
     Uinf, alpha, gamma_1, gamma_2, gamma_3 = float(path[0].split('_')[2]), float(path[0].split('_')[3])*np.pi/180, float(path[0].split('_')[4]), float(path[0].split('_')[5]), float(path[0].split('_')[6])
@@ -36,21 +39,56 @@ if __name__ == "__main__":
     df_train_target = pd.DataFrame(train_data[0].y_train, columns=["u", "v", "p", "nut"])
     df_train = pd.concat([df_train_input, df_train_target], axis=1) 
 
-    df_train_n, _ = normalize(df_train)
-    mean_variance_dict = {column: {"mean": 0.0, "var": 1.0} for i, column in enumerate(df_train.columns)}
-    df_aerofoil = df_train.iloc[len_list[0]:len_list[0]+len_list[1],:]
+    df_train['u_inlet'] = u_inlet
+    df_train['v_inlet'] = v_inlet
+    df_train['gamma_1'] = gamma_1
+    df_train['gamma_2'] = gamma_2
+    df_train['gamma_3'] = gamma_3
 
-    df_freestream_n = df_train_n.iloc[:len_list[0],:]
-    df_aerofoil_n = df_train_n.iloc[len_list[0]:len_list[0]+len_list[1],:]
-    df_box_n = df_train_n.iloc[len_list[0]:len_list[0]+len_list[1]+len_list[2],:]
-        
+    df_box = df_train.iloc[len_list[0]:len_list[0]+len_list[1]+len_list[2],:]
+
+
+    # df = df_box[(df_box['x'] <= 4) & (df_box['x'] >= 1.5)]
+
+    # Define the grid limits
+    x_min, x_max = -2, 4
+    y_min, y_max = -1.5, 1.5
+
+    # Define the resolution of the grid
+    grid_resolution_x = 2000  # Number of points along x
+    grid_resolution_y = 1000  # Number of points along y
+
+    # Create grid points
+    grid_x = np.linspace(x_min, x_max, grid_resolution_x)
+    grid_y = np.linspace(y_min, y_max, grid_resolution_y)
+    grid = np.meshgrid(grid_x, grid_y)
+
+    # Create grid points
+    grid_points = np.vstack([grid[0].ravel(), grid[1].ravel()]).T
+
+    # For demonstration, interpolating 'x_n' feature
+    features = df_box[['x', 'y', "sdf", "x_n", "y_n", 'u_inlet', 'v_inlet', 'gamma_1', 'gamma_2', 'gamma_3']].to_numpy()
+    outputs = df_box[['x', 'y', 'u', 'v', 'p', 'nut']].to_numpy()
+
+    # Perform interpolation
+    features_grid = griddata(features[:, :2], features, grid_points, method='linear')
+    outputs_grid = griddata(features[:, :2], outputs, grid_points, method='linear')
+
+    # Reshape the results to the grid shape
+    interpolated_feature = features_grid.reshape((10, grid_resolution_x, grid_resolution_y))
+    output_interpolated_reshaped = outputs_grid.reshape((6, grid_resolution_x, grid_resolution_y))
+
+    x_y_tensor = torch.from_numpy(interpolated_feature[:2]).float().requires_grad_(True)
+    other_features_tensor = torch.from_numpy(interpolated_feature[2:]).float()
+
+    input_tensor_torch = torch.cat([x_y_tensor, other_features_tensor], dim=0).unsqueeze(0)
+    output_tensor_torch = torch.from_numpy(output_interpolated_reshaped[np.newaxis, ...])
+
     print("Datasets Loaded.")
-
-    from FourierNeuralOperatorNN import FourierNeuralOperatorNN
-    from plot import plot_predictions_vs_test, plot_test
+    from models.PIMNN_Phy_Bc import PIMNN_Phy_Bc
 
     # Train the model
-    model = FourierNeuralOperatorNN(12, 20, df_train, df_aerofoil, mean_variance_dict, len_list, u_inlet, v_inlet, gamma_1, gamma_2, gamma_3)
+    model = PIMNN_Phy_Bc(input_tensor_torch, output_tensor_torch)
     if torch.cuda.device_count() > 1:
         print(f"Let's use {torch.cuda.device_count()} GPUs!")
         model = torch.nn.DataParallel(model)
@@ -58,10 +96,8 @@ if __name__ == "__main__":
     # Check for CUDA availability
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    print(f"Using devide : {device}")
-
     model.to(device)
 
     print(f"Started Training.")
-    model.train(11)
+    model.train(1)
     print(f"Finished Training.")
